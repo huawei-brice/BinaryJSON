a3e8dc32dd76ccc409ed18a696a68fec158acc5f
Dan Appel <dan.appel00@gmail.com>
2016-03-09 11:35:12 -0800

    Fix Xcode setup

--- Tests/BSONTests.swift
+++ Tests/BSONTests.swift
@@ -8,144 +8,146 @@
 
 import XCTest
 import BinaryJSON
-import SwiftFoundation
 import CBSON
 
 class BSONTests: XCTestCase {
-    
+
     var allTests : [(String, () -> Void)] {
         return [
             ("testUnsafePointer", testUnsafePointer),
-            ("testJSONString", testJSONString),
-            ("testJSONEncodable", testJSONEncodable)
         ]
     }
-    
+
     func testUnsafePointer() {
-        
+
         let document = sampleDocument()
-        
+
         // create from pointer
         do {
-            
+
             print("Document: \n\(document)\n")
-            
+
             guard let unsafePointer = BSON.unsafePointerFromDocument(document)
                 else { XCTFail("Could not create unsafe pointer"); return }
-            
+
             defer { bson_destroy(unsafePointer) }
-            
+
             guard let newDocument = BSON.documentFromUnsafePointer(unsafePointer)
                 else { XCTFail("Could not create document from unsafe pointer"); return }
-            
+
             print("New Document: \n\(document)\n")
-            
+
             XCTAssert(newDocument == document, "\(newDocument) == \(document)")
         }
-        
+
         // try to create a 2nd time, to make sure we didnt modify the unsafe pointer
         do {
-            
+
             guard let unsafePointer = BSON.unsafePointerFromDocument(document)
                 else { XCTFail("Could not create unsafe pointer"); return }
-            
+
             defer { bson_destroy(unsafePointer) }
-            
+
             guard let newDocument = BSON.documentFromUnsafePointer(unsafePointer)
                 else { XCTFail("Could not create document from unsafe pointer"); return }
-            
+
             XCTAssert(newDocument == document, "\(newDocument) == \(document)")
         }
     }
-    
-    func testJSONString() {
-        
-        let document = sampleDocument()
-        
-        print("Document: \n\(document)\n")
-        
-        let jsonString = BSON.toJSONString(document)
-        
-        print("JSON: \n\(jsonString)\n")
+
+    func testConvertible() {
+        let document: BSON.Document = [
+            "string": "string",
+            "int": 5,
+            "double": 5.123,
+            "bool": true,
+            "arr": [1,2,3],
+            "dict": [
+                "key1": "value1",
+                "key2": [1,2,3]
+            ]
+        ]
+
+        XCTAssert(document["string"]?.stringValue == "string")
+        XCTAssert(document["int"]?.intValue == 5)
+        XCTAssert(document["double"]?.doubleValue == 5.123)
+        XCTAssert(document["bool"]?.boolValue == true)
+        XCTAssert(document["arr"]!.arrayValue! == [1,2,3])
+        XCTAssert(document["dict"]!.documentValue! == ["key1": "value1", "key2": [1,2,3]])
     }
-    
-    func testJSONEncodable() {
-        
-        let document = sampleDocument()
-        
-        print("Document: \n\(document)\n")
-        
-        let jsonString = BSON.toJSONString(document)
-        
-        print("JSON: \n\(jsonString)\n")
-        
-        #if os(OSX)
-        
-        guard let parsedJSON = JSON.Value(string: jsonString)
-            else { XCTFail("Could not parse JSON string"); return }
-        
-        print("Parsed JSON: \n\(parsedJSON)\n")
-            
-        let convertedJSON = document.toJSON()
-        
-        print("Converted JSON: \n\(convertedJSON)\n")
-        
-        XCTAssert(parsedJSON == convertedJSON, "Converted JSON should equal parsed JSON")
-        
-        #endif
+
+    func testJSON() throws {
+        let document: BSON.Document = [
+            "string": "string",
+            "int": 5,
+            "double": 5.123,
+            "bool": true,
+            "arr": [1,2,3],
+            "dict": [
+                "key1": "value1",
+                "key2": [1,2,3]
+            ]
+        ]
+
+        let json = BSON.toJSONString(document)
+
+        // lazy checking
+        XCTAssert(json.characters.count > 0)
+
+        let documentFromJSON = try BSON.fromJSONString(json)
+
+        XCTAssertEqual(document, documentFromJSON)
     }
 }
 
 // MARK: - Internal
 
 func sampleDocument() -> BSON.Document {
-    
+
     let time = TimeInterval(Int(TimeIntervalSince1970()))
-    
+
     // Date is more precise than supported by BSON, so equality fails
     let date = Date(timeIntervalSince1970: time)
-        
+
     var document = BSON.Document()
-    
+
     // build BSON document
     do {
-        
+
         var numbersDocument = BSON.Document()
-        
+
         numbersDocument["double"] = .Number(.Double(1000.1111))
-        
+
         numbersDocument["int32"] = .Number(.Integer32(32))
-        
+
         numbersDocument["int64"] = .Number(.Integer64(64))
-        
+
         document["numbersDocument"] = .Document(numbersDocument)
-        
+
         document["string"] = .String("Text")
-        
+
         document["array"] = .Array([.Document(["key": .Array([.String("subarray string")])])])
-        
+
         document["objectID"] = .ObjectID(BSON.ObjectID())
-        
-        document["data"] = .Binary(BSON.Binary(data: "test".toUTF8Data()))
-        
+
+        document["data"] = .Binary(BSON.Binary(data: Data(byteValue: [] + "test".utf8)))
+
         document["datetime"] = .Date(date)
-        
+
         document["null"] = .Null
-        
+
         document["regex"] = .RegularExpression(BSON.RegularExpression("pattern", options: ""))
-        
+
         document["code"] = .Code(BSON.Code("js code"))
-        
+
         document["code with scope"] = .Code(BSON.Code("JS code", scope: ["myVariable": .String("value")]))
-        
+
         document["timestamp"] = .Timestamp(BSON.Timestamp(time: 10, oridinal: 1))
-        
-        document["minkey"] = .Key(.Minimum)
-        
-        document["maxkey"] = .Key(.Maximum)
+
+        document["minkey"] = .MaxMinKey(.Minimum)
+
+        document["maxkey"] = .MaxMinKey(.Maximum)
     }
-    
+
     return document
 }
-
-
